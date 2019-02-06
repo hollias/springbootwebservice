27366e322e31e42e641107d12d06432af59fa881
hollias <clous.namsoo@gmail.com>
2019-02-06 10:47:16 +0900

    first

--- src/main/java/me/hollias/webservice/domain/posts/Posts.java
+++ src/main/java/me/hollias/webservice/domain/posts/Posts.java
@@ -1,9 +1,12 @@
 package me.hollias.webservice.domain.posts;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import me.hollias.webservice.domain.BaseTimeEntity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -11,11 +14,25 @@ import javax.persistence.Id;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(length = 500, nullable = false)
     private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
+
     private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
 }
--- src/main/java/me/hollias/webservice/web/WebRestController.java
+++ src/main/java/me/hollias/webservice/web/WebRestController.java
@@ -1,12 +1,28 @@
 package me.hollias.webservice.web;
 
+import lombok.AllArgsConstructor;
+import me.hollias.webservice.domain.posts.PostsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    //@Autowired
+    PostsRepository postsRepository;
+
     @GetMapping("/hello")
     public String hello(){
         return "Hello World";
     }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
+    }
+
 }
